47d545c36a00e43d429808454f99a1861a66f507
Aaron <simota@mail.gvsu.edu>
2021-11-23 14:38:25 +0000

    Fixed Shiny App

--- Baseball/app.R
+++ Baseball/app.R
@@ -7,15 +7,20 @@ library(tools)
 
 # Load data --------------------------------------------------------------------
 
-load(here::here("Baseball/MVPClean1960_2020.Rdata"))
-load(here::here("Baseball/CYClean1960_2020.RData"))
-load(here::here("Baseball/RookieClean1960_2020.RData"))
+# load MVP data
+mvp <- read_csv2(here::here("Baseball/MVPClean1960_2020"))
+# Load Cy Young Data
+cy <- read_csv2(here::here("Baseball/CYClean1960_2020"))
+# Load Rookie Data
+rook <- read_csv2(here::here("Baseball/RookieClean1960_2020"))
 
 # Define UI --------------------------------------------------------------------
 
 ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
+            
+            # Input selector for Y-axis
             selectInput(
                 inputId = "y",
                 label = "Y-axis:",
@@ -32,16 +37,17 @@ ui <- fluidPage(
                     "Saves" = "SV",
                     "Earn Run Average" = "ERA",
                     "Innings Pitched" = "IP",
-                    "Strike Outs" = "SO",
+                    "Strike Outs" = "SO"
                 ),
                 selected = "WAR"
             ),
+            
             
             selectInput(
                 inputId = "x",
                 label = "Season:",
                 choices = c(
-                    "Season" = "Year",
+                    "Season" = "Year"
                 ),
                 selected = "Year"
             ),
@@ -73,40 +79,39 @@ ui <- fluidPage(
                 value = 2
             ),
             
-            textInput(
-                inputId = "plot_title",
-                label = "Player Stats",
-                #        placeholder = "Enter text to be used as plot title"
-            ),
+            # textInput(
+            #     inputId = "plot_title",
+            #     label = "Player Stats",
+            #     #        placeholder = "Enter text to be used as plot title"
+            # ),
             
             #      actionButton(
             #        inputId = "update_plot_title",
             #        label = "Update plot title"
             #      )
         ),
-        
-#        mainPanel(
-#            tags$br(),
-#            ___, # add text here
-#            plotOutput(outputId = "scatterplot")
+
+        mainPanel(
+            plotOutput(outputId = "scatterplot")
         )
     )
+)
 
 
 # Define server ----------------------------------------------------------------
 
 server <- function(input, output, session) {
-    new_plot_title <- eventReactive(
-        eventExpr = input$update_plot_title,
-        valueExpr = {
-            toTitleCase(input$plot_title)
-        }
-    )
+    # new_plot_title <- eventReactive(
+    #     eventExpr = input$update_plot_title,
+    #     valueExpr = {
+    #         toTitleCase(input$plot_title)
+    #     }
+    # )
     
+    # Generate scatterplot for awardee Statistics# Generate scatterplot for awardee Statistics
     output$scatterplot <- renderPlot({
-        ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
-            geom_point(alpha = input$alpha, size = input$size) +
-            labs(title = new_plot_title())
+        ggplot(data = mvp, aes_string(x = input$x, y = input$y)) +
+            geom_point()
     })
 }
 
