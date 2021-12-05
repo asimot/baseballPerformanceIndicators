4b0289ff4ebc1180697fcfcf01f060e328624f16
Aaron <simota@mail.gvsu.edu>
2021-12-05 20:14:32 +0000

    Remodeled Batter Stat Scatter Plot

--- Baseball/app.R
+++ Baseball/app.R
@@ -10,13 +10,13 @@ library(tools)
 # Load data --------------------------------------------------------------------
 
 # load MVP data
-mvp <- as_tibble(read.csv2(here::here("Baseball/CopyOfMVPClean1960_2020")))
+mvp <- as_tibble(read.csv2(here::here("Baseball/MVPClean1960_2020")))
 
 # Load Cy Young Data
-cy <- as_tibble(read.csv2(here::here("Baseball/CopyOfCYClean1960_2020")))
+cy <- as_tibble(read.csv2(here::here("Baseball/CYClean1960_2020")))
 
 # Load Rookie Data
-rook <- as_tibble(read.csv2(here::here("Baseball/CopyOfRookieClean1960_2020")))
+rook <- as_tibble(read.csv2(here::here("Baseball/RookieClean1960_2020")))
 
 # Loading in the teams data for pitchers and batters
 teamsBat <- read_csv2(here::here("Baseball/cleanTeamsBat1900_2020"))
@@ -42,34 +42,41 @@ ui <- fluidPage(
             
             # Input selector for Y-axis
             selectInput(
-                inputId = "y",
-                label = "Y-axis:",
+                inputId = "baty",
+                label = "Batter Statistic:",
                 choices = c(
-                    "Average" = "AVG",
-                    "Batting Average" = "BA",
-                    "On Base Percentage" = "OBP",
-                    "Slugging" = "SLG",
-                    "Home Runs" = "HR",
+                    "Games Played" = "G",
+                    "Plate Appearances" = "PA",
+                    "At Bats" = "AB",
+                    "Runs Scored" = "R",
+                    "Hits" = "H",
+                    "Singles Hit" = "`1B`",
+                    "Doubles Hit" = "`2B`",
+                    "Triples Hit" = "`3B`",
+                    "Home Runs Hit" = "HR",
                     "Runs Batted In" = "RBI",
                     "Stolen Bases" = "SB",
-                    "Wins" = "W",
-                    "Loses" = "L",
-                    "Saves" = "SV",
-                    "Earn Run Average" = "ERA",
-                    "Innings Pitched" = "IP",
-                    "Strike Outs" = "SO"
+                    "Caught Stealing" = "CS",
+                    "Bases on Balls" = "BB",
+                    "Strikeouts" = "SO",
+                    "Double Plays Grounded Into" = "GDP",
+                    "Times Hit by Pitch" = "HBP",
+                    "Sacrifice Hits" = "SH",
+                    "Sacrifice Flies" = "SF",
+                    "Intentional Bases on Balls" = "IBB",
+                    "Battting Average" = "AVG"
                 ),
                 selected = "AVG"
             ),
             
-            selectInput(
-                inputId = "x",
-                label = "Season:",
-                choices = c(
-                    "Season" = "Year"
-                ),
-                selected = "Year"
-            ),
+            # selectInput(
+            #     inputId = "x",
+            #     label = "Season:",
+            #     choices = c(
+            #         "Season" = "Year"
+            #     ),
+            #     selected = "Year"
+            # ),
             
             selectInput(
                 inputId = "z",
@@ -114,14 +121,18 @@ server <- function(input, output, session) {
     
     # Generate scatterplot for player Statistics
     output$scatterplot <- renderPlot({
-        ggplot(data = playerBat, aes_string(x = playerBat$Season, y = input$y)) +
-            geom_point()
+        ggplot(data = playerBat, 
+               aes_string(x = playerBat$Season, y = input$baty)) +
+            geom_point() + 
+            labs(
+                title = paste0("Batter Stats For ", str_replace_all(input$baty, "`", "")),
+                subtitle = "1960 to 2020"
+            ) + 
+            xlab("Season") +
+            ylab(str_replace_all(input$baty, "`", ""))
     })
 
 
-    Selected_var <- reactive(mvp[[input$z]])
-    Bw <- reactive((max(Selected_var())-min(Selected_var()))/mean(Selected_var()))
-    
     # Histogram of Batting Average density across MVP Hitters
 
     output$histogram <- renderPlot({
@@ -129,19 +140,11 @@ server <- function(input, output, session) {
             # Removing pitchers from displayed data
             filter(is.na(ERA)) %>%
             ggplot(mapping = aes_string(x = input$z)) + 
-
-
-            geom_histogram(binwidth = Bw(), color = "black", fill = "blue") + 
-
-
             geom_histogram(
                 binwidth = input$binwidth, 
                 color = "black", 
                 fill = "blue"
                 ) + 
-
-
-
             geom_density(color = "red") + 
             labs(
                 title = paste0("Stats for ", awardName() , " Winners"),
