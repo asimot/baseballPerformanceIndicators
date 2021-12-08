544e36555e3968d58c120e2fb4dd5ec8748b1eb2
Aaron <simota@mail.gvsu.edu>
2021-12-08 22:10:44 +0000

    Cleaned up

--- Baseball/app.R
+++ Baseball/app.R
@@ -122,31 +122,16 @@ ui <- fluidPage(
             # Distribution of player stats by year
             plotOutput(outputId = "playerDistrib"),
             
-<<<<<<< HEAD
-            # Distribution of MVP and Rookie stats
-
-            plotOutput(outputId = "histogram"),
-
-            # Searchable player stats
-            reactableOutput(outputId = "playerTable"),
-
-
-=======
             # Awardee distribution histogram
->>>>>>> eb62985516a181fcfc6a0d9bdc6358f637c12445
             plotOutput(outputId = "awardees"),
             
-            strong("Searchable Individual Batter Statistics Data sourced from Fangraphs.com", align = "center"),
+            strong("Searchable Individual Batter Statistics [Data sourced from Fangraphs.com]", align = "center"),
             # Searchable player batting stats
             reactableOutput(outputId = "playerBatTable"),
             
-            strong("Searchable Individual Pitcher Statistics Data sourced from Fangraphs.com", align = "center"),
+            strong("Searchable Individual Pitcher Statistics [Data sourced from Fangraphs.com]", align = "center"),
             # Searchable player pitching stats
             reactableOutput(outputId = "playerPitchTable")
-<<<<<<< HEAD
-
-=======
->>>>>>> eb62985516a181fcfc6a0d9bdc6358f637c12445
         )
     )
 )
